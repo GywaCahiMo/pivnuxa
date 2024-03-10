303b3a41e6d5f09eb30b8c130ea7c1c3785916bd
GywaCahiMo <nikita-l-1987@mail.ru>
2024-03-10 19:47:59 +0300

    aplicationActivity

--- app/src/main/java/com/example/pivnyxa/applicationActivity.kt
+++ app/src/main/java/com/example/pivnyxa/applicationActivity.kt
@@ -1,11 +1,35 @@
 package com.example.pivnyxa
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class applicationActivity : AppCompatActivity() {
+    private lateinit var textPhone: EditText
+    private lateinit var textAddress : EditText
+    private lateinit var Products : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_application)
+
+        textPhone = findViewById(R.id.editTextPhone)
+        textAddress = findViewById(R.id.editTextAddress)
+
+    }
+    fun sendARequest(view: View){
+        val dbHelper = MyDbHelper(this)
+        val db = dbHelper.writableDatabase
+
+        val values = ContentValues().apply {
+            put(MyDbHelper.COLUMN_ID, 1)
+            put(MyDbHelper.COLUMN_PHONE, textPhone.text.toString())
+            put(MyDbHelper.COLUMN_ADDRESS, textAddress.text.toString())
+            put(MyDbHelper.COLUMN_PRODUCTS, Products)
+        }
+        db.insert(MyDbHelper.TABLE_NAME, null, values)
     }
 }

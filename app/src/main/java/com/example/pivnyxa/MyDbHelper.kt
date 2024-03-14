82bb2397181b93e73308dc4f6b7b9883ade65470
GywaCahiMo <nikita-l-1987@mail.ru>
2024-03-14 22:00:47 +0300

    ubtate

--- app/src/main/java/com/example/pivnyxa/MyDbHelper.kt
+++ app/src/main/java/com/example/pivnyxa/MyDbHelper.kt
@@ -1,12 +1,24 @@
 package com.example.pivnyxa
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class MyDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_NAME = "UserData.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "UserTable"
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_PHONE = "phone"
+        private const val COLUMN_ADDRESS = "address"
+        private const val COLUMN_PRODUCTS = "products"
+    }
+
     override fun onCreate(db: SQLiteDatabase) {
-        val createTable = "CREATE TABLE $TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_PHONE TEXT, $COLUMN_ADDRESS TEXT, $COLUMN_PRODUCTS TEXT)"
+        val createTable = "CREATE TABLE $TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_PHONE TEXT, $COLUMN_ADDRESS TEXT,$COLUMN_PRODUCTS TEXT)"
         db.execSQL(createTable)
     }
 
@@ -14,15 +26,13 @@ class MyDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nu
         db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
         onCreate(db)
     }
-
-    companion object {
-        const val DATABASE_NAME = "mydatabase.db"
-        const val DATABASE_VERSION = 1
-        const val TABLE_NAME = "mytable"
-        const val COLUMN_ID = "id"
-        const val COLUMN_PHONE = "phone"
-        const val COLUMN_ADDRESS = "address"
-        const val COLUMN_PRODUCTS= "products"
+    fun addingAnApplication(phone: String, address: String, products: String ) {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(COLUMN_PHONE, phone)
+        values.put(COLUMN_ADDRESS, address)
+        values.put(COLUMN_PRODUCTS, products)
+        db.insert(TABLE_NAME, null, values)
+        db.close()
     }
 }
-data class MyData(val id: Int, val phone: String, val address: String, val products: String)
--- app/src/main/java/com/example/pivnyxa/applicationActivity.kt
+++ app/src/main/java/com/example/pivnyxa/applicationActivity.kt
@@ -12,9 +12,9 @@ import com.example.pivnyxa.MainActivity.Companion.totalPrice
 class applicationActivity : AppCompatActivity() {
     private lateinit var textPhone: EditText
     private lateinit var textAddress : EditText
-    companion object{
-        var Products : String = ""
-    }
+    private lateinit var Products: Array<String>
+    var ProductsAll: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_application)
@@ -25,18 +25,27 @@ class applicationActivity : AppCompatActivity() {
         val textTotalPrice: TextView = findViewById(R.id.textViewTotalPrice)
         textTotalPrice.text = totalPrice.toString()
 
+        Products = Array(33, { "" })
+
+        val sharedPref = getSharedPreferences("MyApp", MODE_PRIVATE)
+        for (i in Products.indices) {
+            if(i <= 19){
+                Products[i] = sharedPref.getString("textView_$i", "0").toString()
+            }
+            if(i in 20..27){
+                Products[i] = sharedPref.getString("textViewSnaks_$i", "0").toString()
+            }
+            if(i > 27){
+                Products[i] = sharedPref.getString("textViewGlasses_$i", "0").toString()
+            }
+        }
+        for (i in Products.indices){
+            ProductsAll += Products[i] + " "
+        }
     }
     fun sendARequest(view: View){
-        val dbHelper = MyDbHelper(this)
-        val db = dbHelper.writableDatabase
-
-        val values = ContentValues().apply {
-            put(MyDbHelper.COLUMN_ID, 1)
-            put(MyDbHelper.COLUMN_PHONE, textPhone.text.toString())
-            put(MyDbHelper.COLUMN_ADDRESS, textAddress.text.toString())
-            put(MyDbHelper.COLUMN_PRODUCTS, Products)
-        }
-        db.insert(MyDbHelper.TABLE_NAME, null, values)
+        val dbHelper = DatabaseHelper(this)
+        dbHelper.addingAnApplication(textPhone.text.toString(), textAddress.text.toString(),ProductsAll)
 
         // Восстанавливаем состояние TextView, если оно было сохранено ранее
         val sharedPref = getSharedPreferences("MyApp", Context.MODE_PRIVATE)
@@ -44,15 +53,4 @@ class applicationActivity : AppCompatActivity() {
         editor.clear()
         editor.apply()
     }
-    /*
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        // Сохраняем состояние TextView перед уничтожением активити
-        val sharedPref = getSharedPreferences("MyApp", Context.MODE_PRIVATE)
-        with (sharedPref.edit()) {
-            putInt("totalPrice", totalPrice)
-            apply()
-        }
-    }
-     */
 }
